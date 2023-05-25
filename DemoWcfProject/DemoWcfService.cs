efd41c1006392b61714dddf952f7a345fb1de628
Kodpencerem <emrullah04@outlook.com>
2023-05-25 13:26:02 +0300

    Add project files.

---
+++ DemoWcfProject/DemoWcfService.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoWcfProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DemoWcfService" in both code and config file together.
+    public class DemoWcfService : IDemoWcfService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ DemoWcfProject/IDemoWcfService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace DemoWcfProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDemoWcfService" in both code and config file together.
+    [ServiceContract]
+    public interface IDemoWcfService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
